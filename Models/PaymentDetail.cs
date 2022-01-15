00289e8cc52250a29c95c978636d125d56813313
jujeonghwan <jujeonghwan@gmail.com>
2022-01-15 15:31:49 -0500

    Add project files.

---
+++ Models/PaymentDetail.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * PaymentDetail.cs
+ * PaymentAPI
+ * ASP.NET Core Web API CRUD with Angular 11 (https://www.youtube.com/watch?v=S5dzfuh3t8U)
+ * 
+ *  Revision History
+ *      Jeonghwan Ju, 2022.01.15: Created
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PaymentDetailId { get; set; }
+
+        [Column(TypeName ="nvarchar(100)")]
+        public string CardOwnerName { get; set; }
+
+        [Column(TypeName = "nvarchar(16)")]
+        public string CardNumber { get; set; }
+                
+        [Column(TypeName = "nvarchar(5)")]
+        public string ExpirationDate { get; set; }
+
+        [Column(TypeName = "nvarchar(3)")]
+        public string SecurityCode { get; set; }
+
+    }
+}
